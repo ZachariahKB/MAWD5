3475596431578bb3bd4ff7e908c523249266f7ca
ZachariahKB <zachariahakb@gmail.com>
2024-05-11 15:29:11 -0700

    overdue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // Retrieve tasks and nextId from localStorage
+const myModal = document.getElementById('formModal')
+const myInput = document.getElementById('myInput')
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
@@ -34,5 +36,10 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
+    
+    myModal.addEventListener('shown.bs.modal', () => {
+      myInput.focus()
+      console.log ("clicked")
+    })
 });
